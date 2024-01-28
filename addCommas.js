25c19ec2c1ea25ff7e7d3209f53cb64435c9df49
denz93 <>
2024-01-28 14:47:35 -0800

    implement part2 addCommas

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,41 @@
-function addCommas() {}
+function addCommas(num) {
+  if (Number.isInteger(num)) {
+    return formatInterger(num)
+  }
+
+  if (isFloat(num)) {
+    return formatInterger(Math.floor(Math.abs(num)) * Math.sign(num)) + '.' + formatDecimal(num)
+  }
+  return ""
+}
+
+function formatInterger(num) {
+  const strNum = num.toString()
+  const len = strNum.length
+  let idx = len - 3;
+  let result = ""
+  while (idx > 0) {
+    result = ',' + strNum.slice(idx, idx + 3) + result
+    idx -= 3
+  }
+  result = strNum.slice(0, 3+idx) + result
+  return result
+}
+
+function formatDecimal(num) {
+  const strNum = num.toString()
+  const parts = strNum.split('.')
+  if (parts.length === 1) {
+    return '0'
+  }
+  return parts[1]
+}
+
+
+function isFloat(num) {
+  const strNum = num.toString()
+  const parts = strNum.split('.')
+  return parts.length === 2 && parts[1].length > 0 && parts[0].length > 0
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,41 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("it returns a string", () => {
+    expect(typeof addCommas(1)).toBe("string");
+  });
+  
+  test("it returns 1,234", () => {
+    expect(addCommas(1234)).toBe("1,234");
+  })
+
+  test("it returns 1,000,000", () => {
+    expect(addCommas(1000000)).toBe("1,000,000");
+  })
+
+  test("it returns 9,876,543,210", () => {
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+  })
+
+  test("it returns 6", () => {
+    expect(addCommas(6)).toBe("6");
+  })
+
+  test("it returns -10", () => {
+    expect(addCommas(-10)).toBe("-10");
+  })
+
+  test("it returns -5678", () => {
+    expect(addCommas(-5678)).toBe("-5,678");
+  })
+
+  test("it returns 12,345.678", () => {
+    expect(addCommas(12345.678)).toBe("12,345.678");
+  })
+
+  test("it returns -3,141,592.65", () => {
+    expect(addCommas(-3141592.65)).toBe("-3,141,592.65");
+  })
+
 });
